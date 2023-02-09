b62104a553cf774f48b1425778e9da93e715ec17
Transcription Factory <transcriptionfactory@gmail.com>
2023-02-09 12:40:38 -0500

    not sure why didn't commit earlier

--- src/LASSO.py
+++ src/LASSO.py
@@ -1,3 +1,12 @@
+
+# check scripts in this folder, particularly LASSO_Grid.R
+# /ix/djishnu/Aaron/LASSOEST/src/LASSO_Grid.R
 import pandas as pd
 
 
+# Lasso CV from scikit-learn (replaces cv.glmnet in R)
+# /ix/djishnu/Aaron/LASSOEST/src/LASSO_Grid.R
+# https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LassoCV.html#sklearn.linear_model.LassoCV
+
+# Lasso (replaces glmnet in R)
+# https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.Lasso.html#sklearn.linear_model.Lasso
